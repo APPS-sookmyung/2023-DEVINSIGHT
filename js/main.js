da00e30a49706f5370339963309dbe2f8729cf06
misung.dev <misung.dev@gmail.com>
2023-08-25 01:43:05 +0900

    Feat: 카드 컴포넌트 개발

---
+++ js/main.js
@@ -0,0 +1,55 @@
+function createCard({ name, imageUrl, linkUrl, title, description }) {
+	const card = document.createElement("a");
+	card.className = "col";
+	card.href = linkUrl;
+
+	const cardInner = document.createElement("div");
+	cardInner.className = "card h-100";
+
+	const cardImg = document.createElement("img");
+	cardImg.className = "card-img-top";
+	cardImg.src = imageUrl;
+
+	const cardBody = document.createElement("div");
+	cardBody.className = "card-body";
+
+	const cardTitle = document.createElement("h6");
+	cardTitle.className = "card-title";
+	cardTitle.textContent = `[${name}] ${title}`;
+
+	const cardText = document.createElement("p");
+	cardText.className = "card-text";
+	cardText.textContent = description;
+
+	card.appendChild(cardInner);
+	cardInner.appendChild(cardImg);
+	cardInner.appendChild(cardBody);
+	cardBody.appendChild(cardTitle);
+	cardBody.appendChild(cardText);
+
+	return card;
+}
+
+function main() {
+	fetch("../database/devNewsList.json")
+		.then((response) => response.json())
+		.then((devNewsList) => {
+			console.log(devNewsList);
+
+			const devNewsSectionElement = document.querySelector(".dev-news-section");
+
+			devNewsList.forEach(({ name, imageUrl, linkUrl, title, description }) => {
+				const devNewsCard = createCard({
+					name,
+					imageUrl,
+					linkUrl,
+					title,
+					description,
+				});
+
+				devNewsSectionElement.appendChild(devNewsCard);
+			});
+		});
+}
+
+main();
