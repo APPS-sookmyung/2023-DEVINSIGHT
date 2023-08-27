4fdace3fed005591e7cf4d4d7d58819014c2a7bc
misung.dev <misung.dev@gmail.com>
2023-08-28 00:40:59 +0900

    Delete: 컨텐츠 카드 description 제거

--- js/main.js
+++ js/main.js
@@ -1,10 +1,13 @@
-function createCard({ name, imageUrl, linkUrl, title, description }) {
+function createCard({ name, imageUrl, linkUrl, title }) {
 	const card = document.createElement("a");
 	card.className = "col";
 	card.href = linkUrl;
 
 	const cardInner = document.createElement("div");
 	cardInner.className = "card h-100";
+
+	const cardImgwrapper = document.createElement("div");
+	cardImgwrapper.className = "card-img-wrapper";
 
 	const cardImg = document.createElement("img");
 	cardImg.className = "card-img-top";
@@ -17,15 +20,11 @@ function createCard({ name, imageUrl, linkUrl, title, description }) {
 	cardTitle.className = "card-title";
 	cardTitle.textContent = `[${name}] ${title}`;
 
-	const cardText = document.createElement("p");
-	cardText.className = "card-text";
-	cardText.textContent = description;
-
 	card.appendChild(cardInner);
-	cardInner.appendChild(cardImg);
+	cardInner.appendChild(cardImgwrapper);
+	cardImgwrapper.appendChild(cardImg);
 	cardInner.appendChild(cardBody);
 	cardBody.appendChild(cardTitle);
-	cardBody.appendChild(cardText);
 
 	return card;
 }
@@ -33,21 +32,18 @@ function createCard({ name, imageUrl, linkUrl, title, description }) {
 function main() {
 	fetch("../database/devVideoList.json")
 		.then((response) => response.json())
-		.then((devNewsList) => {
-			console.log(devNewsList);
+		.then((devVideoList) => {
+			const devVideoSectionElement = document.querySelector(".dev-video-section");
 
-			const devNewsSectionElement = document.querySelector(".dev-news-section");
-
-			devNewsList.forEach(({ name, imageUrl, linkUrl, title, description }) => {
-				const devNewsCard = createCard({
+			devVideoList.forEach(({ name, imageUrl, linkUrl, title }) => {
+				const devVideoCard = createCard({
 					name,
 					imageUrl,
 					linkUrl,
 					title,
-					description,
 				});
 
-				devNewsSectionElement.appendChild(devNewsCard);
+				devVideoSectionElement.appendChild(devVideoCard);
 			});
 		});
 }
